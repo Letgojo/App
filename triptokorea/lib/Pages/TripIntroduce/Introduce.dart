a368c103f15fb31a95b45ee6824640a14d856535
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-03-14 12:17:37 +0900

    🚀feat:지역축제소개

--- triptokorea/lib/Pages/TripIntroduce/Introduce.dart
+++ triptokorea/lib/Pages/TripIntroduce/Introduce.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Introduce extends StatelessWidget {
+class Introduce extends StatefulWidget {
   const Introduce({super.key});
 
   @override
+  State<Introduce> createState() => _IntroduceState();
+}
+
+class _IntroduceState extends State<Introduce> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Introduce")),
+      body: SingleChildScrollView(
+          child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 80),
+              width: 250,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(children: [
+                Text(
+                  "기간검색",
+                  style: GoogleFonts.jua(
+                      textStyle: TextStyle(fontSize: 20, color: Colors.black)),
+                ),
+                Row(
+                  children: [],
+                )
+              ]),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
