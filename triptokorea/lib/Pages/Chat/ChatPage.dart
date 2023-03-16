c5281a9eb287c1eabf9492e45b1feadbe9edf0de
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-03-16 11:56:29 +0900

    🚀feat:채팅레이아웃 구성

--- triptokorea/lib/Pages/Chat/ChatPage.dart
+++ triptokorea/lib/Pages/Chat/ChatPage.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Chat")),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Chat',
+          style: GoogleFonts.jua(
+              textStyle: TextStyle(
+                  fontSize: 20,
+                  decoration: TextDecoration.underline,
+                  color: Colors.black)),
+        ),
+      ),
     );
   }
 }
