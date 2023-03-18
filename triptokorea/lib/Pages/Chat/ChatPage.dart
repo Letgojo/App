b287e350cf6fd28bcb721d80a090278c657bfb86
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2023-03-18 15:22:58 +0900

    Revert "👍feat:채팅레이아웃구성"
    This reverts commit 6f907971e1e440c4f3a27440d819bdd34d5ac033.

--- triptokorea/lib/Pages/Chat/ChatPage.dart
+++ triptokorea/lib/Pages/Chat/ChatPage.dart
@@ -12,44 +12,18 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          centerTitle: true,
-          title: Text(
-            '채팅',
-            style: GoogleFonts.jua(
-                textStyle: TextStyle(fontSize: 20, color: Colors.black)),
-          ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Chat',
+          style: GoogleFonts.jua(
+              textStyle: TextStyle(
+                  fontSize: 20,
+                  decoration: TextDecoration.underline,
+                  color: Colors.black)),
         ),
-        body: SingleChildScrollView(
-            child: Container(
-          child: Column(
-            children: [
-              Container(
-                height: 80,
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "검색어을 입력해주세요",
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10)),
-                      //돋보기 아이콘
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          print("돋보기 크기 클릭");
-                        },
-                        icon: Icon(Icons.search),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )));
+      ),
+    );
   }
 }
