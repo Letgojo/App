7636c025ea7e1f586b33c985282ff5045b3dfad7
lovaoi777 <lovaoi777@naver.com>
2023-04-06 18:05:03 +0900

    fix: 구글맵API로 수정

--- triptokorea/lib/Pages/Trip/TripYesPage/TripYes_result.dart
+++ triptokorea/lib/Pages/Trip/TripYesPage/TripYes_result.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kakaomap_webview/kakaomap_webview.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 const String kakaoMapKey = '5157004dd04f0e8b82c4ba27aac81564';
 
@@ -11,38 +11,24 @@ class TripYes_result extends StatefulWidget {
 }
 
 class _TripYes_resultState extends State<TripYes_result> {
-  final double _lat = 35.886779;
-  final double _lng = 128.653534;
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(35.8535156, -128.5431268);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                  height: 302,
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      KakaoMapView(
-                        width: double.infinity,
-                        height: 300,
-                        kakaoMapKey: kakaoMapKey,
-                        lat: 35.886779,
-                        lng: 128.653534,
-                        showMapTypeControl: true,
-                        showZoomControl: true,
-                        draggableMarker: true,
-                        mapType: MapType.BICYCLE,
-                      )
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1),
-                  )),
-            ],
+      body: Container(
+        height: 300,
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
           ),
         ),
       ),
